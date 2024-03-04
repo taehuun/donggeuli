2604e538495e2ec07d6b7ed342c29d00f1db6c5d
김민주 <ducco705@snu.ac.kr>
2024-03-04 17:56:03 +0900

    :tada: Initial commit

---
+++ frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3e8d585ce81182b19cfd292ebeabf21f6864871057061e0f6f239d38bcd81df5
+size 121
---
+++ frontend/test/widget_test.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c931276e6c551af96ab687e0f331d461b28d686883328da5a6e67a1096b2bbc9
+size 1059
