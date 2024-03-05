e5330299c480f36507d7bf811a3cbb22d9ed340e
김민주 <ducco705@snu.ac.kr>
2024-03-05 17:53:18 +0900

    :sparkles: feat: #S10P22C101-76 이미지 추가
    Former-commit-id: 441cc15e95c1e654870d8c9b4311b1075e56d3ca

--- frontend/lib/core/theme/constant/app_icons.dart
+++ frontend/lib/core/theme/constant/app_icons.dart
@@ -1,5 +1,3 @@
-class AppIcons {
-
-  static const String donggle = "assets/images/donggle.png";
-  static const String background = "assets/images/background.png";
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:bb08c8540b57856859b8d8b3973e7abfe54460f829eeda05db37c49e167da7c5
+size 1233
