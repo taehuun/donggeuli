14b9680606cefaad2fc195641b4bed4ee8050a3f
김민주 <ducco705@snu.ac.kr>
2024-03-06 11:44:40 +0900

    :sparkles: feat: #S10P22C101-76 기본화면 및 테마 설정
    Former-commit-id: ad074ba965be9eb795814fb47940cd2179767621

--- frontend/lib/presentation/pages/home/home_page.dart
+++ frontend/lib/presentation/pages/home/home_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbab10244fa591dc9003bd9c0f69dad2ff10a1de6e3da8c5da70a4c7207f6178
-size 1952
+oid sha256:43b8fc16ce28882a7de0c154260098e778fc070c72c90a945716ad8c28a146ed
+size 674
