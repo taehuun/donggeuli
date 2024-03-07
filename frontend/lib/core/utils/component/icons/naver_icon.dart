298d0e42e40f10fe943785bbdaadb8b575baae95
김민욱 <minwookkim@naver.com>
2024-03-07 14:38:45 +0900

    ✨ feat: #S10P22C101-75 로그인 화면 소셜로그인 버튼 구현
    Former-commit-id: a6c58ef7cac036c27b0f67e6d4dc1de9dda3d402

---
+++ frontend/lib/core/utils/component/icons/naver_icon.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa3c0fd70567daf6a342c811f67419832e2dc8fb94020d6dd1e6e47cbd21d5b9
+size 388
--- frontend/lib/presentation/pages/login/login_page.dart
+++ frontend/lib/presentation/pages/login/login_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34d32d4f7cef70693f84a8a9d201984375baf3a60489d92b7fe4d098621783fd
-size 8289
+oid sha256:35b0bcc79edfb0cc90b7b13ff8b4cce385c5b050637f266916ef2b276340ec32
+size 9108
