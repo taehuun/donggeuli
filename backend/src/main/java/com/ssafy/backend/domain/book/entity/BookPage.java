1268c32da294218e3e4c3e3e70e2252dd5fe2c8f
강대수 <sore503@naver.com>
2024-03-08 17:58:25 +0900

    :sparkles: feat : #S10P21C101-109 동화책 상세 구현 중
    Former-commit-id: b85a93c659aeff356ff36e436a3eaaaf0649894a

--- backend/src/main/java/com/ssafy/backend/domain/book/entity/BookPage.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/entity/BookPage.java
@@ -1,4 +1,3 @@
-package com.ssafy.backend.domain.book.entity;
-
-public class BookPage {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:72685162537bae077dce6f90625ee9f820e3aa43982ecf8af6550028e9ba8ea6
+size 612
--- backend/src/main/java/com/ssafy/backend/domain/book/entity/BookPageSentence.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/entity/BookPageSentence.java
@@ -1,4 +1,3 @@
-package com.ssafy.backend.domain.book.entity;
-
-public class BookPageSentence {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a27b3de1dc4e83a1014bff2b38fc1d79f420479fb459dfb4191b3e8eb8d2537
+size 672
