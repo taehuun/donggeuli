b93a21a1655c9892fad8cc99308c1c4e09fa583e
강대수 <sore503@naver.com>
2024-03-08 13:53:05 +0900

    ✨feat : #S10P22C101-111 동화책 전체 목록 조회 구현
    Former-commit-id: 6d81cfc1168bef0987a572a9b7ad704a89d781db

--- backend/src/main/java/com/ssafy/backend/domain/book/entity/Book.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/entity/Book.java
@@ -1,4 +1,3 @@
-package com.ssafy.backend.domain.book.entity;
-
-public class Book {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:693cf1f00db2e48989ae0e03261047d224a6822fa86008a1a05cfe2fde94a362
+size 548
