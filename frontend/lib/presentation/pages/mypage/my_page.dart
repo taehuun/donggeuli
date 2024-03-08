780a0c9d1d1835798fff4a0793411fa1f5db4d64
김민주 <ducco705@snu.ac.kr>
2024-03-08 09:42:47 +0900

    :sparkles: feat: #S10P22C101-76 상태관리를 통한 마이페이지 탭 변경
    Former-commit-id: b3c79a3209ff7388e964ce1e11886bf738442a04

--- frontend/lib/presentation/pages/mypage/my_page.dart
+++ frontend/lib/presentation/pages/mypage/my_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64e5f95addf6b9d35533131a4c8be38adbe1d8c91d209269c5f16da2efe6ff06
-size 1042
+oid sha256:aa1d7971c482ba87ee34d9d7e2296b501e798fd3e3766fbd352acde3a955806f
+size 1144
