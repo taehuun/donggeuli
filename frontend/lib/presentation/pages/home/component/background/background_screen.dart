6bd1bd7adc9265be05d65ea5905e9505174e4db8
김민욱 <minwookkim@naver.com>
2024-03-08 15:38:12 +0900

    ✨ feat: #S10P22C101-75 배경화면 애니메이션 적용
    Former-commit-id: f363204f4eea36464089997f75a03fc041202f11

--- frontend/lib/presentation/pages/home/component/background/background_screen.dart
+++ frontend/lib/presentation/pages/home/component/background/background_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a81088a1631149cf977afceb4e98c28f7ad3c7b1a393b79cd352224318c38a7
-size 22706
+oid sha256:8baf1997a7e53de9f9193660bcc5466a5df7b78c54b2b4e35f791b9a589fbf6d
+size 36139
