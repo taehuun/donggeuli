d3b3bb30f4ed8329fc0ada163fa8962431f62470
김민석 <kms278711@naver.com>
2024-03-11 16:08:31 +0900

    :bug: fix #S10P22C101-102 리프레쉬 토큰 기능 수정
    Former-commit-id: 18aa09ca06228797d705fa92aaa48dbc9fdfadb7

--- backend/src/main/java/com/ssafy/backend/domain/user/dto/ReissueDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/dto/ReissueDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record ReissueDto (
 
-    @JsonProperty("refresh_token")
     String refreshToken
 )
 { }
--- backend/src/main/java/com/ssafy/backend/domain/user/service/impl/AuthServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/service/impl/AuthServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70cacb60bff17c7e353f7e76a7a50140f0394845d19cacab2212543ce0aeefa0
-size 3802
+oid sha256:2f28f6b5c20d98abdd8587ff6a329ce84c833e37dc5989ef6fae1060556f19ce
+size 5487
