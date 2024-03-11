fdba093a8366a0d9b7e5099d5ee05f2c07ee7145
김민욱 <minwookkim@naver.com>
2024-03-11 15:40:15 +0900

    feat: #S10P22C101-191 로그아웃 모달 구성중
    Former-commit-id: a88aa9d338335df45dcd695aa48232d2e8add55a

--- frontend/lib/presentation/pages/modal/signout_modal.dart
+++ frontend/lib/presentation/pages/modal/signout_modal.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:459b6d68586ea288b0b6a42d0ddf81ec5b0c5cba0ea661904b97855efd113b80
-size 343
+oid sha256:7bfdc7e9add6459c24bf630e3716af439dbb9096c85562ac9db46c4fab46afad
+size 376
