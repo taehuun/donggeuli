47b88ee911ea60f70a29c603f4577c4f74a1e58c
김민주 <ducco705@snu.ac.kr>
2024-03-12 11:30:26 +0900

    test
    Former-commit-id: cea2424609654e4558a42bb487590db713804cc7

--- frontend/lib/core/utils/component/icons/home_icon.dart
+++ frontend/lib/core/utils/component/icons/home_icon.dart
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5b32353e884b28e5f1e33c210fd642de8359bcf2807da535d09c52941fefdfcd
-size 385
+import 'package:flutter/material.dart';
+import 'package:frontend/core/theme/constant/app_icons.dart';
+
+class HomeIcon extends StatelessWidget {
+  const HomeIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Image.asset(AppIcons.home_icon,
+          width: MediaQuery.of(context).size.width * 0.05),
+    );
+  }
+}
--- frontend/lib/presentation/pages/mypage/my_page_background.dart
+++ frontend/lib/presentation/pages/mypage/my_page_background.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a397a7b30a90814731e12a62ac550943cf302ec83cd36c5b06222333db5342f
-size 5975
+oid sha256:2be75f466c765770e6bcab4f79f3b9a445bf6a57e51e018e5d1d517111e8b458
+size 6022
