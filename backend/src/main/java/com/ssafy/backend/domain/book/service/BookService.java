decf0fa1dd2dd7dfb3f0d5b4f74c7fc5c9c30c64
강대수 <sore503@naver.com>
2024-03-12 16:37:03 +0900

    ✨feat: #S10P21C101-258-1 진행중인 책 페이지 저장
    Former-commit-id: 4a91ed60698e6681430cdb9c1f5596491e735ef5

--- backend/src/main/java/com/ssafy/backend/domain/book/service/BookService.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/service/BookService.java
@@ -1,15 +1,3 @@
-package com.ssafy.backend.domain.book.service;
-
-import com.ssafy.backend.domain.book.dto.BookDto;
-import com.ssafy.backend.domain.book.dto.BookPageDto;
-import com.ssafy.backend.domain.book.dto.BookPageSentenceDto;
-
-import java.util.List;
-
-public interface BookService {
-
-    List<BookDto> searchAllBook();
-    BookDto searchBook(Long bookId);
-    BookPageDto searchBookPage(Long bookId, int page);
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:483a7c949832a7486eb37f9579ef386f64fa0f257cec6d2c31662d9f4e844748
+size 589
