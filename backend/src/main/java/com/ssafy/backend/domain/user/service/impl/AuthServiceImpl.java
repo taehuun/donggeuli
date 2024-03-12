0efa48328523af7d1efaf59d1158065eacf7b8e6
김민석 <kms278711@naver.com>
2024-03-12 15:49:26 +0900

    🐛 fix: User 테이블 구조 변경

--- backend/src/main/java/com/ssafy/backend/domain/user/service/impl/AuthServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/service/impl/AuthServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f28f6b5c20d98abdd8587ff6a329ce84c833e37dc5989ef6fae1060556f19ce
-size 5487
+oid sha256:f60d5a8f8f2fc86fa69746457b6899184647dfc836e3900202bc33e3425db209
+size 5492
