cb9d5ecf53cbd978fac0f668e3d83317fe8a4705
김민욱 <minwookkim@naver.com>
2024-03-12 14:35:52 +0900

    ✨ feat: #S10P22C101-190 provider 사용, 정보 수정 페이지 구현 중
    Former-commit-id: 1c029cfe80992202894a0b576059cd10a1201ba9

--- frontend/lib/core/theme/custom/custom_font_style.dart
+++ frontend/lib/core/theme/custom/custom_font_style.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c691c76566d663edac333a2e5ec267d6f405df5e623a4c991077755709a3cce4
-size 2101
+oid sha256:673c4a28b25c4e0dd867f0320c2817aa4494b1fb9f848d7fe50c151c51667cd2
+size 2438
