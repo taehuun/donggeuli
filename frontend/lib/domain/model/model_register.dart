b7cd723dc4cdae30a75f7d335155a7d6ea5e42ec
김민주 <ducco705@snu.ac.kr>
2024-03-12 16:19:32 +0900

    :sparkles: feat: #S10P22C101-262 회원가입 기능 구현 완료
    Former-commit-id: 03918cf0d38808d44e21bf893097ced8ab38ddbf

--- frontend/lib/domain/model/model_register.dart
+++ frontend/lib/domain/model/model_register.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02bb34c5ad34f488202278a1a6b377e99038e663eea6c0aee3688bba7ea9b173
-size 1890
+oid sha256:0ca4b8b9254a6cc67f8a72cfbdcb620166e83e5ebae4f9ecbfebd3e8e188a0c7
+size 2170
