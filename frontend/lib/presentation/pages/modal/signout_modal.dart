68533732268151264697ce7849118b562dfe8ccf
김민욱 <minwookkim@naver.com>
2024-03-12 14:35:52 +0900

    ✨ feat: #S10P22C101-190 provider 사용, 정보 수정 페이지 구현 중
    Former-commit-id: f32db5bf88e6f0ed505f62864cab7c35789ce87b

--- frontend/lib/presentation/pages/modal/signout_modal.dart
+++ frontend/lib/presentation/pages/modal/signout_modal.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f227e60b1e20ee85675000c3d834c3a1a90a58ca0a3bbde43e9b08a2388b462
-size 1007
+oid sha256:7a5e60c8b3091e8504c8a05691db0fcbdc4ea2fbc87807b1f1647cdb837f45f0
+size 1543
--- frontend/lib/presentation/pages/mypage/my_page_background.dart
+++ frontend/lib/presentation/pages/mypage/my_page_background.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73830edd574078811bb450d331a5eaede6efc9f509b3f6376d76e45b8b6eba98
-size 6281
+oid sha256:4633e8f748633d94f7af89af5ce2cd589d3bff17e5c0af30a7d2abe2da873e65
+size 6279
