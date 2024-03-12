518f9c6999bf39e1cdf3fe13281a792511ec83ae
김민욱 <minwookkim@naver.com>
2024-03-12 11:37:55 +0900

    ✨ feat: #S10P22C101-190 provider 사용, 정보 수정 페이지 구현 중
    Former-commit-id: 5a851af5c0380322bdc79f37d09d921b1b1b33a8

--- frontend/lib/presentation/pages/mypage/my_page_background.dart
+++ frontend/lib/presentation/pages/mypage/my_page_background.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d69c2d9d142c14e8281d54b4b54a2f3a3dc5a3e3372c97a986d4813c9bac558
-size 5999
+oid sha256:4474d69c886cfd3caba22f09472c3975ee56b22673480cbd9c5336c07a22897c
+size 6147
