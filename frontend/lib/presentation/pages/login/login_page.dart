31fcefadf0de57ab69d0ba806c7ed1a147f039e1
김민주 <ducco705@snu.ac.kr>
2024-03-12 17:30:42 +0900

    :sparkles: feat: #S10P22C101-261 자동 로그인 기능 구현 완료
    Former-commit-id: e905989d41d491c8ceed7af3fb05ec1282c65685

--- frontend/lib/presentation/pages/login/login_page.dart
+++ frontend/lib/presentation/pages/login/login_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52fc4908e0b9c06bb61bdbc5e7cc088db480d14185dd8745173d0a6810f76b4f
-size 12678
+oid sha256:52b3236dc4f00b459854a49f2247c37d7284b61c5547d99feecdf4dfe6bf372b
+size 12716
