3b740fac1691a5a308028108e446f2947c1ae089
김민주 <ducco705@snu.ac.kr>
2024-03-12 14:40:10 +0900

    :construction: feat: #S10P22C101-262 로그인 기능 구현 중
    Former-commit-id: 830c48e88627ede4c4ba90d2696082478d3d7680

--- frontend/lib/core/theme/custom/custom_font_style.dart
+++ frontend/lib/core/theme/custom/custom_font_style.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c691c76566d663edac333a2e5ec267d6f405df5e623a4c991077755709a3cce4
-size 2101
+oid sha256:ca7017caccc0c55ac69b0bb52cf69e5ab0b749e0b14142681bfb3d0dbc86dbb0
+size 2269
