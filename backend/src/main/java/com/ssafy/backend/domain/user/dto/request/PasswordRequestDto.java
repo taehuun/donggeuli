a7483024580363a0b2039f6f3196a8c930b6ce78
김민석 <kms278711@naver.com>
2024-03-12 16:37:03 +0900

    🐛 fix: 닉네임, 비밀번호 변경 기능 수정
    Former-commit-id: 59ac36c57c8cc9be2dd0434742d14c8252479ea2

--- backend/src/main/java/com/ssafy/backend/domain/user/dto/request/PasswordRequestDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/dto/request/PasswordRequestDto.java
@@ -1,9 +1,5 @@
 package com.ssafy.backend.domain.user.dto.request;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record PasswordRequestDto (
 
     String currentPassword,
--- backend/src/main/java/com/ssafy/backend/domain/user/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/service/impl/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c74eacee95914dc624a0a42fa480a9c4bdba2a287800b2057f68375aed845d1
-size 2486
+oid sha256:1575255d1c009dfeaf0767b6a18e9d6756e5746a0ab17159cfba77d92e15915d
+size 2629
