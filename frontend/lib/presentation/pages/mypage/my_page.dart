842e6f969b2d341c4296d75b33bbd5e42bc370c0
김민주 <ducco705@snu.ac.kr>
2024-03-12 23:57:33 +0900

    :construction: feat: #S10P22C101-165 내 정보 확인 화면 구성 중...
    Former-commit-id: 4dfa8d1affea34e3bda0c6745b0f8aa01c39e2e2

--- frontend/lib/presentation/pages/mypage/my_page.dart
+++ frontend/lib/presentation/pages/mypage/my_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cf3f527ee1bab3dabc38594079f8b245580af3fe5e67f079ebea0101349af61
-size 5014
+oid sha256:4be61c6bbfb67ed4c16d327dbdf3fd163f060c274dbead3eea9788886cb71359
+size 5428
--- frontend/lib/presentation/provider/user_provider.dart
+++ frontend/lib/presentation/provider/user_provider.dart
@@ -1,29 +1,3 @@
-class UserProvider{
-  String _email = "";
-  String _access_token = "";
-  String _refresh_token = "";
-
-  void setEmail(String email){
-    _email = email;
-  }
-
-  void setAccessToken(String accessToken){
-    _access_token = accessToken;
-  }
-
-  void setRefreshToken(String refreshToken){
-    _refresh_token = refreshToken;
-  }
-
-  String getEmail(){
-    return _email;
-  }
-
-  String getAccessToken(){
-    return _access_token;
-  }
-
-  String getRefreshToken(){
-    return _refresh_token;
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0b67b2d24e98251960538a8a58605cff5232cf3327fb18d54453cfd3e709a1b
+size 2299
