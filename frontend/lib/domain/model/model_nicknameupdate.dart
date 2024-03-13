34d78d0eca3a9ebc927cc56f6222146bca43f7b4
김민욱 <minwookkim@naver.com>
2024-03-13 14:53:42 +0900

    ✨feat: #S10P21C101-190 정보수정 구현중
    Former-commit-id: edf3506ee0a02bade709d5866fd1998808b29560

--- frontend/lib/domain/model/model_nicknameupdate.dart
+++ frontend/lib/domain/model/model_nicknameupdate.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43cd1c33027b2984552547870ec9db55921bfe3a64af7a5b37d1acfccae47be7
-size 461
+oid sha256:bc56f73ec85cb5d92c81ebeb24a53c359fedfdb893a00703a8c03a76f842a7ac
+size 1210
--- frontend/lib/presentation/pages/modal/nickname_update_modal.dart
+++ frontend/lib/presentation/pages/modal/nickname_update_modal.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:137eb47f11f14636662c9438a9bfcfdfa869081b27783d73a9ae848d4a67faa3
-size 1479
+oid sha256:7f939772004d458b10492a75d87e9fd89864f42702f7ce8ed079829fbc73ab1f
+size 2986
