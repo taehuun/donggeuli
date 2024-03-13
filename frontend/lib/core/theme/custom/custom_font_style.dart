8d44f7b8fd7c3263ecd31ebf8e92af055bb9d21a
김민욱 <minwookkim@naver.com>
2024-03-13 13:25:24 +0900

    ✨feat: #S10P21C101-190 정보수정 구현중
    Former-commit-id: e27f8063a6be07b9bdf5f7b5e11d09264590d893

--- frontend/lib/core/theme/custom/custom_font_style.dart
+++ frontend/lib/core/theme/custom/custom_font_style.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbfd637e230ab2007732bc4992a405d23ccf4eff513133fac93de050d39b40f4
-size 2606
+oid sha256:dd291c99b4c8e4c63ad91082ec11ea4ae1133d89593c58d057ee013495003a80
+size 2780
