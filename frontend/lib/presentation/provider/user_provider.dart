a878e3241a989f9b2d25c74ad08e2fc70de3fd27
김민주 <ducco705@snu.ac.kr>
2024-03-13 09:11:20 +0900

    :sparkles: feat: #S10P22C101-165 내 정보 확인 구현 완료
    Former-commit-id: 2f0c645ed30a0e26ae18470ee052e1a1bc6e03fe

--- frontend/lib/presentation/provider/user_provider.dart
+++ frontend/lib/presentation/provider/user_provider.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0b67b2d24e98251960538a8a58605cff5232cf3327fb18d54453cfd3e709a1b
-size 2299
+oid sha256:e6abcd23c3bdab6de3488125b2b870771535a2caf886f12013692b1ac229225e
+size 2324
