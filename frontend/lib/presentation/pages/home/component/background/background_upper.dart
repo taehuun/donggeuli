b10d2044ed776dbcbe21654d04134c00357c965b
김민주 <ducco705@snu.ac.kr>
2024-03-13 20:45:14 +0900

    :construction: feat: #S10P22C101-198 애니메이션 제거
    Former-commit-id: 3d51400fb9269580f61207f7e689e56d68f96e26

--- frontend/lib/presentation/pages/home/component/background/background_upper.dart
+++ frontend/lib/presentation/pages/home/component/background/background_upper.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f471b0a90878f3314be8822068e0eb46a0c8ebb54c342ca9fd3b16406d988f4
-size 33896
+oid sha256:6db6fb1d0f0f7bd352078da880ee61ed3e12660aad3b4695236868e869433350
+size 36361
