da8efa99215cd6de704a16751866a0026af5b960
강대수 <sore503@naver.com>
2024-03-15 17:03:17 +0900

    ✨feat: #S10P21C101-117 책에 남겨진 리뷰 조회 구현
    Former-commit-id: 62764892a4e5f22efaf989c73c38346c90faf184

--- backend/src/main/java/com/ssafy/backend/domain/book/controller/BookController.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/controller/BookController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f847acba6c8144fb86aa485f255bbd47b7da69bfe00fec7be397b7cc29b65839
-size 4293
+oid sha256:ddd962bc9210e31732136ce0fd0879a5c33346f8723e8ead93568492158dc193
+size 4678
