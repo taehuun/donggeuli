c74ef7da1d3a037443384550434d7183b0e29a8d
김민욱 <minwookkim@naver.com>
2024-03-15 17:08:58 +0900

    ✨feat: #S10P21C101-190 프로필이미지 변경 수정중
    Former-commit-id: c8198ecf8b2f901dc85e0f487a923e198a5b12aa

--- frontend/lib/presentation/pages/mypage/my_page_update.dart
+++ frontend/lib/presentation/pages/mypage/my_page_update.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c525d3c9de23c2283ec3f6be1ef0c9250a3967c9bfed896d693043b113f9d5d
-size 8656
+oid sha256:edbb8b2d48f49559cf559a49f283b64b95dd7c4ebbc782148a9e61e7c4a2d0e0
+size 8662
