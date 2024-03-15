8269544045d83665f148e6c4653c7a95ecfd6042
강대수 <sore503@naver.com>
2024-03-15 14:10:24 +0900

    ♻feat: #S10P21C101-117 진행중인 책 조회, 결제창책조회 코드 수정
    Former-commit-id: 75eba83fdd7ae62a5987779adb4cc84086c8b89d

--- backend/src/main/java/com/ssafy/backend/domain/book/entity/BookReview.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/entity/BookReview.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:954be71f5c68e4124e67465f2ad97c8fd1a34dbb99e35a0245d21931e6cee409
-size 720
+oid sha256:8936ef784bfe9309cb2a959e38e3121709da31d867c3edd01381802b31c9b3a0
+size 754
--- backend/src/main/java/com/ssafy/backend/domain/book/service/impl/BookServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/service/impl/BookServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a17358a7ed9ed6f58ebd0024fd3a0e70f1cf713ef4c4774f939678657c48a14e
-size 7919
+oid sha256:dd92d09423acc55f183fb9160652a5272822b974f031edd40f653806f8e13873
+size 7938
