ec623ca87b1c15c3399a0cbebb06491eff1700e5
김민주 <ducco705@snu.ac.kr>
2024-03-15 17:56:32 +0900

    Border.all(
    Former-commit-id: c8a7505271a43d8cc4a3183f80d425a755b92ba4

--- frontend/lib/domain/model/model_books.dart
+++ frontend/lib/domain/model/model_books.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b03bb7abbc93848edfeb404c4d0d04829eba3c95e082e5b123cd0bf5af93e588
-size 2513
+oid sha256:9a4132b79ae077246f4ed723e110667d88d0cfe9ded2c14b0235cdab8de5d97f
+size 2545
--- frontend/lib/presentation/pages/mypage/Book/book_detail.dart
+++ frontend/lib/presentation/pages/mypage/Book/book_detail.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bea1df8e4a2f32f64ba3773ffc96f045e006b82e7c9a6694aa72b96087f66f5
-size 1671
+oid sha256:34f29935cc182f2bb581a4d935af62a1470cdedbff3a6bbcd2681300a7b2e183
+size 4967
