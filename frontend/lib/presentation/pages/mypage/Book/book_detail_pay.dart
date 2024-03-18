abe89603955a907978879e83f028aa9f1203e8b8
김민주 <ducco705@snu.ac.kr>
2024-03-18 17:50:53 +0900

    added todo
    Former-commit-id: 2348ec8ceff33d584c3b8cab649a81352d01008e

--- frontend/lib/presentation/pages/mypage/Book/book_detail_pay.dart
+++ frontend/lib/presentation/pages/mypage/Book/book_detail_pay.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc8d395e03c4106c90613e404ce0ddd897feef12462faa34a704ddd6b262a9ab
-size 7630
+oid sha256:1d4276a5a3ee2c186b5055fe67c01d021ec2c95ee4e6ea84cee72654d6f8550c
+size 8085
