29ea9b9e2324010c8791f1a01c6ce45c0796fe72
김민욱 <minwookkim@naver.com>
2024-03-18 17:47:29 +0900

    ✨feat: #S10P21C101-275 단어 상세 페이지 비동기 요청 수정중
    Former-commit-id: 4e98d777e7d43ff071ac71e9d277d2720d5be5f0

--- frontend/lib/domain/model/model_cards.dart
+++ frontend/lib/domain/model/model_cards.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9fe060a90c82ae8b5b222df65d8b91cb920904e32b5c9254a8e4e13ecf74633
-size 1533
+oid sha256:624385acc626faf182210500463e8a53adad936694e54a4153e3b6fa3e42851a
+size 2410
--- frontend/lib/presentation/pages/card/opened_card.dart
+++ frontend/lib/presentation/pages/card/opened_card.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:feffe7714b3ef8bf365f89632b26f6a65dc989642533dd77aad47bfd43755a41
-size 1397
+oid sha256:e9035456a125779cd1017e1f2840b5095ca605f0d7b1529d00c7a4ab1276f5cb
+size 2001
