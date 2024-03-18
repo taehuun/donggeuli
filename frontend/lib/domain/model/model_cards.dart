9a38db12c64aa3eea2af8a7a0093575d0007b753
김민욱 <minwookkim@naver.com>
2024-03-18 15:31:18 +0900

    ✨feat: #S10P21C101-275 단어 불러오기 api 완료
    Former-commit-id: db5105c6668ce753115b5a0a2f950ac36b0b2285

---
+++ frontend/lib/domain/model/model_cards.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a9fe060a90c82ae8b5b222df65d8b91cb920904e32b5c9254a8e4e13ecf74633
+size 1533
---
+++ frontend/lib/presentation/pages/card/opened_card.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:feffe7714b3ef8bf365f89632b26f6a65dc989642533dd77aad47bfd43755a41
+size 1397
