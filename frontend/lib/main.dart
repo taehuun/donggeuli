de3f087ea61af0e0ad5c13819c0b229794b29be0
김민주 <ducco705@snu.ac.kr>
2024-03-20 00:23:30 +0900

    minor change
    Former-commit-id: d584c628d2fa7167133aeb48b3e09efad27e1b96

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e185750d179ea83bba1ae32f782e80796db87b285064c4577d990ba5d7b560b
-size 5574
+oid sha256:ce4dcce8363be5afd34343d0d0eb3d5e137afd6a1b257f643160564a002a3e9f
+size 5519
