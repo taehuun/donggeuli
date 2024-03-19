3d3477b78db511417384ecb8ec98ff51fb731fbd
김민욱 <minwookkim@naver.com>
2024-03-19 11:00:06 +0900

    ✨feat: #S10P21C101-275 단어 상세페이지 구현
    Former-commit-id: f41a7857ae6ac745f42e1f463a4cc1739ad12d27

--- frontend/lib/presentation/pages/card/card_deatil.dart
+++ frontend/lib/presentation/pages/card/card_deatil.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ccacd767de1a6d19b55a29dc664f4b5c639b4d1dfbe2c4f8b81b9b2cf4454be
-size 5330
+oid sha256:c36f53d01d22d0ae69ba492178ba5ccf17653ca1ae02e2d45e9878e00dbfb7d5
+size 8494
