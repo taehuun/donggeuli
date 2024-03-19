8fa6c8836495951d34cd1d7de4fe704a65ff7828
김민석 <kms278711@naver.com>
2024-03-19 15:34:16 +0900

    🐛 fix : 퀴즈 목록조회 requestparam으로 변경2
    Former-commit-id: 83f8d9c6fbdc481b488d54e3d828fdea00d62fde

--- backend/src/main/java/com/ssafy/backend/domain/quiz/service/QuizService.java
+++ backend/src/main/java/com/ssafy/backend/domain/quiz/service/QuizService.java
@@ -1,11 +1,11 @@
 package com.ssafy.backend.domain.quiz.service;
 
-import com.ssafy.backend.domain.quiz.dto.request.QuizRequestDto;
 import com.ssafy.backend.domain.quiz.dto.response.QuizResponseDto;
+import com.ssafy.backend.domain.quiz.entity.WordQuiz;
 
 import java.util.List;
 
 public interface QuizService {
 
-    List<QuizResponseDto> getQuiz(QuizRequestDto quizRequestDto, Long userId);
+    List<QuizResponseDto> getQuiz(WordQuiz.Theme theme, Long bookId, Long userId);
 }
--- backend/src/main/java/com/ssafy/backend/domain/quiz/service/impl/QuizServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/quiz/service/impl/QuizServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a8c8264c5056473434ca49296b2f3c9ad4c35e6f0be2d07ae7e2e70440a5d4f
-size 1410
+oid sha256:bec4d221d446929c31c457e0d5c787c2d08336faaf7ef16a5e64e54c796115c3
+size 1281
