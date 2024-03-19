57baa7ff0c0815b52948ff6fd5d06fefd536a10f
강대수 <sore503@naver.com>
2024-03-19 16:04:04 +0900

    ♻️ refactor: #S10P21C101-backend 책 평점 수정
    Former-commit-id: 1b7334c52af66d2fcb1c72f83abd7148c777f6e3

--- backend/src/main/java/com/ssafy/backend/domain/book/dto/request/BookReviewRequestDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/dto/request/BookReviewRequestDto.java
@@ -1,10 +1,15 @@
 package com.ssafy.backend.domain.book.dto.request;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import lombok.Builder;
 
 @Builder
 public record BookReviewRequestDto(
-        int score,
+
+        @DecimalMin(value = "0.0")
+        @DecimalMax(value = "5.0")
+        double score,
         String content
 ) {
 }
