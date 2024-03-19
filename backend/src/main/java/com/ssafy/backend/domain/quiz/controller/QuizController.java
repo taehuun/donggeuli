75d3d062229bb58509ef76313c86ed9295ddcddf
김민석 <kms278711@naver.com>
2024-03-19 15:29:06 +0900

    🐛 fix : 퀴즈 목록조회 requestparam으로 변경
    Former-commit-id: bbc5a56803418e79fa284321fba456783da42ead

--- backend/src/main/java/com/ssafy/backend/domain/quiz/controller/QuizController.java
+++ backend/src/main/java/com/ssafy/backend/domain/quiz/controller/QuizController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7aca2f1ec53c2c14f04992abdc168b5c9c9b4a2454d5f6389a8c567cbb92e10
-size 1346
+oid sha256:1641a30345d7a6fa19ce30febf17c787394ad82d67361804318ea7de50b6b6d9
+size 1152
