1de813e00def2c9dda4bc99f5d9600fc0814cde6
김민석 <kms278711@naver.com>
2024-03-19 13:46:35 +0900

    🐛 fix : 컨설턴트님 피드백 사항 반영(쓰지 않는 변수, return 정리)
    Former-commit-id: f6ad85a58ae8bc76aa668ae00c86f3da54a05761

--- backend/src/main/java/com/ssafy/backend/global/jwt/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/ssafy/backend/global/jwt/security/JwtAuthenticationFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b899494bb3ca1f76145e4cfa28a6dd5f9657439257feec8205eb054eb2063c0
-size 4158
+oid sha256:15284c97fda4df26ffd462be13290a2145f0a78fda5f394c1bba493bc5b375e6
+size 4118
