04ee35e6b1e2a20291d08d6d2ad86190abf170ce
강대수 <sore503@naver.com>
2024-03-19 15:38:24 +0900

    ♻refactor: #S10P21C101-backend 평점 스코어 수정
    Former-commit-id: 79d6e466ad0f51aea66d9a7f53163bcf3fa7b72f

--- backend/src/main/java/com/ssafy/backend/domain/book/dto/response/BookReviewMyResponseDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/dto/response/BookReviewMyResponseDto.java
@@ -1,14 +1,3 @@
-package com.ssafy.backend.domain.book.dto.response;
-
-import lombok.Builder;
-
-@Builder
-public record BookReviewMyResponseDto(
-        Long bookId,
-        Long userId,
-        String title,
-        String coverPath,
-        int score,
-        String content
-) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a9ea00a6498666c0b56f6312201692fbfa39d0e3913da41244b9e9a93a2cf93
+size 266
