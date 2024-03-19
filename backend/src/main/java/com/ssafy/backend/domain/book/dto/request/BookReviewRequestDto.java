12cd423b9b37eb47da6446959e707d9f8b7c3e99
강대수 <sore503@naver.com>
2024-03-19 16:04:04 +0900

    ♻️ refactor: #S10P21C101-backend 책 평점 수정
    Former-commit-id: 568f68cf8a7aabdc97fea54347fda5c9bdf426ff

--- backend/src/main/java/com/ssafy/backend/domain/book/dto/request/BookReviewRequestDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/dto/request/BookReviewRequestDto.java
@@ -1,10 +1,3 @@
-package com.ssafy.backend.domain.book.dto.request;
-
-import lombok.Builder;
-
-@Builder
-public record BookReviewRequestDto(
-        int score,
-        String content
-) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:fc82c32432f3834aa345863f8360e7f5b13d0274af2c063493df66d87c61b940
+size 343
