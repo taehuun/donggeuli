3e51943f0bf6f200ff84a6ab5ab5539edcdec46e
김민석 <kms278711@naver.com>
2024-03-19 11:25:41 +0900

    🐛 fix : 테이블 컬럼 변경(작성, 변경일자 삭제)
    Former-commit-id: 048f4bcd0b7917f169661549494268061b6f3f5c

--- backend/src/main/java/com/ssafy/backend/domain/book/entity/BookPage.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/entity/BookPage.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f01ff47aa18374e841982581ff98f6e62ebb9b8ab0d11913d6b6ad0e091f7137
-size 684
+oid sha256:8e7a730c3ec8184bae5424a64720dd963ad9d29f9718bb9d9b89ad2bb0e2af0c
+size 665
