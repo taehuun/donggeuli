f1372c025fd77ba241fc3bc171b165ffa4412e84
김민석 <kms278711@naver.com>
2024-03-20 11:03:37 +0900

    🐛 fix : #S10P22C101-313 퀴즈 순서 랜덤으로 변경
    Former-commit-id: db9bbbaa0bf245359eaf80b1ffc6ce785d56cafc

--- backend/src/main/java/com/ssafy/backend/domain/quiz/service/impl/QuizServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/quiz/service/impl/QuizServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bec4d221d446929c31c457e0d5c787c2d08336faaf7ef16a5e64e54c796115c3
-size 1281
+oid sha256:1d78be486daca3ef2d84b9d6f4deaf234e2d799c320beffb0c67291e441c03a6
+size 1256
