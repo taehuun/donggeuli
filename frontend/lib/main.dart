f323380711381bf65f20fd6976d70f69b33dd701
김민주 <ducco705@snu.ac.kr>
2024-03-22 08:29:18 +0900

    :construction: feat: #S10P22C101-182 결제 진행중
    Former-commit-id: 5f20a6eb98397921fba3be7c10015c076fd29a2d

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4da9feff37855ff9b2e8ee84b4db460c2c79d7a605fb9ca40963cee49494fd19
-size 5282
+oid sha256:ad32f78704354c3462c498e4285520fd3fdd3ed9a60c14687d06ced66bb357e2
+size 5388
