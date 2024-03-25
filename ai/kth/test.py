53d661c562fdd43128d5e8d4f40ca5e8eb37b704
김태훈 <xogns5@naver.com>
2024-03-25 11:02:12 +0900

    🧪test: 배포test8
    Former-commit-id: 7f9ce02764a8d07f9d66fb0d9727180083289c5c

--- ai/kth/test.py
+++ ai/kth/test.py
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a50e93c2018923194363e8337fb6039b29096412135637fa5398fd697bd2c3d8
-size 200
+import tensorflow as tf
+from keras.models import load_model
+
+
+
+doodle_model = tf.models.load_model('./app/model/doodle_cnn.h5')
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
