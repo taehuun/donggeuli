23952a4dbc9abe041ba1c38905652dfbfc7b701b
김민주 <ducco705@snu.ac.kr>
2024-03-25 18:01:39 +0900

    :sparkles: feat: #S10P22C101-208 단어장 구현중
    Former-commit-id: 11e89d3e59907960ee71e6658e0893c2ab125774

--- frontend/lib/presentation/pages/home/component/book/book_detail.dart
+++ frontend/lib/presentation/pages/home/component/book/book_detail.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1f43d2f3a17f60f4123fe4bc8d6c1b054cd9612eddb2c34d67fbf135a17c008
-size 8671
+oid sha256:be7e8fd6c980395271dc71201e26914c3109f06f1d59a7e4af27e9ca455f6af4
+size 8518
