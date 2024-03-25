489576e7efeeb56df0bee30f0b963ac8fa6dfc30
김민석 <kms278711@naver.com>
2024-03-25 17:55:17 +0900

    ✨feat : #S10P22C101-285 네이버 oauth 구현4
    Former-commit-id: 83d2cb5cdde24592fbef152cabdb66313aeb778f

--- backend/src/main/java/com/ssafy/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/backend/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d8d3ed3bbe12dd114108ea4652b5aa59143a196600c032e88faea6aeaf1dc0e
+oid sha256:79a982cb9ea68be7e14c749fce02acaa939b2a90f67904d9c45a95102ec138ed
 size 2933
