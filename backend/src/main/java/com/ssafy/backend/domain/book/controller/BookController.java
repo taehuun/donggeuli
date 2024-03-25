00cf198dc9d220abe82dca6a7842b52a6393af7f
강대수 <sore503@naver.com>
2024-03-25 16:37:18 +0900

    ♻ refactor:#S10P21C101-339 코드리펙토링, 책 조회 수정
    Former-commit-id: 89e6451f98bf5d5d0477138b6900c172b76f15c2

--- backend/src/main/java/com/ssafy/backend/domain/book/controller/BookController.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/controller/BookController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1eaebc8dd0acc4076153950dee318e934f7b638b09767d2a6e4fa5b2bd9181a1
-size 4246
+oid sha256:e3667de848e8dc5c6af6050229756dd72601ed76a5f3b3e7660b785214eb7b52
+size 4374
--- backend/src/main/java/com/ssafy/backend/domain/book/dto/BookInfoDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/dto/BookInfoDto.java
@@ -10,7 +10,7 @@ public record BookInfoDto(
         Long bookId,
         String title,
         String coverImagePath,
-        int page,
+        int processPage,
         List<EducationDto> educations
 ) {
 }
