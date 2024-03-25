5159547117016b8910c7b2c9f2c652b9dc712634
김태훈 <xogns5@naver.com>
2024-03-25 09:50:00 +0900

    🧪test: 배포test6
    Former-commit-id: 59a348d9c981297d277c49ad0388a1f9303679df

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d286d0c9947963d3ba0297626dda8f95c68815c192beb5830481558179275fd7
-size 3095
+oid sha256:319f84f779ce6885e927a5c7f1fdf970f806f7afa4e8823ccf5727e052ff5d4a
+size 3098
