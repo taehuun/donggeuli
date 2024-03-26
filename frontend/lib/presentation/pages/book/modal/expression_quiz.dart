f67ee68f5091e4e114f25bf258afafb01998ac7c
김민주 <ducco705@snu.ac.kr>
2024-03-26 11:51:33 +0900

    :sparkles: feat: #S10P22C101-209 표정인식 퀴즈 화면 구성
    Former-commit-id: f9a7ba44a58fffc50f185e07d993eda3678916e5

--- frontend/lib/presentation/pages/book/modal/expression_quiz.dart
+++ frontend/lib/presentation/pages/book/modal/expression_quiz.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9125950b9f1353f577cbb83c44a415c45d4e414b6c260ddf567778872420d774
-size 5203
+oid sha256:73ee36483ea0e1ba263f6793929bfd376bd4293d22fbcf97da935ded4c99dccc
+size 5184
