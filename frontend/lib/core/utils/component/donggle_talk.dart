eb1746dbc6dfddd39d75214a8e7ec8a9befd5ae9
김민욱 <minwookkim@naver.com>
2024-03-26 16:13:19 +0900

    ✨feat: #S10P21C101-253 동글이 말풍선 완료
    Former-commit-id: 044df4d46ac03e4e81a5c77f5178e054cade5f2b

--- frontend/lib/core/utils/component/donggle_talk.dart
+++ frontend/lib/core/utils/component/donggle_talk.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79890145400343c854496194fa9da9ae820e3a72fc9617d6eea719fa6fc94c86
-size 8855
+oid sha256:1e21a507cd8c284c1ad456803a38017b17bec1cceb784c3cb4fcd4c51e276fdd
+size 12424
