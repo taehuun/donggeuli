b928799e1cbcc0299b2b84241e0abc84413fa2fc
강대수 <sore503@naver.com>
2024-03-27 11:15:40 +0900

    ♻ refactor #S10P21C101-338 isRead 관련 내용 추가
    Former-commit-id: ff2c8c1d824602a2a358ae8fe06b68785d861361

--- backend/src/main/java/com/ssafy/backend/domain/book/repository/bookprocess/UserBookProcessCustomRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/repository/bookprocess/UserBookProcessCustomRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3517143d8025dfa275703e2c92540e0d5bd33435d048f0a4b773b03c17130fb
-size 2089
+oid sha256:34340dddbc5acd791a3626c303308b7b8df7d9767971befc38f68aaf0f74a0d6
+size 2090
--- backend/src/main/java/com/ssafy/backend/domain/book/service/impl/BookServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/service/impl/BookServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a822ed52a67f000bae1bf61547705d3224d06291bc5aa8cb8086d323eb41dde7
-size 9405
+oid sha256:58a6591a3875cd95e10e2a08766023ad4afe95a6490d28a9cb8cae81ae1b6108
+size 10433
