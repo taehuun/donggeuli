031b001401f70fe20722e4147fccaa35a03198f7
김민주 <ducco705@snu.ac.kr>
2024-03-27 14:47:49 +0900

    :sparkles: feat: #S10P22C101-219 배경음악 해결 완료
    Former-commit-id: ff5de71c48415cb51b332f2ce31bbd8dac3efe02

--- frontend/lib/presentation/pages/home/home_page.dart
+++ frontend/lib/presentation/pages/home/home_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da5f954a34f7b817db39aee85beab186bae2f3f94cf994bbf74590a8cd76a5f5
-size 4650
+oid sha256:b564b4a2b65d22d62cdef43a2c6153e62abd69db6c6ac48a9fd10e1b0fc78dfb
+size 4483
