55e308b671abd8bfe2b245fc80c6437646877ce5
김민석 <kms278711@naver.com>
2024-03-27 17:13:51 +0900

    🐛 fix : #S10P22C101-370 action_learning 테이블 스킵여부 컬럼 제거2
    Former-commit-id: 1ca1d9c5e0a9db3c2816607a41f37340ae663465

--- backend/src/main/java/com/ssafy/backend/domain/user/service/UserService.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a102702897e675e954ae7cf28c2926f7055cf4fd0f672d45d9fd2188ffffa072
-size 951
+oid sha256:e2da7e55c7e56d36638b122a758979a10983c47d3dab5933850e7824bb83cb32
+size 932
