ed694b50c67f48728b18bb23be4cec9134d5d26f
김민욱 <minwookkim@naver.com>
2024-03-28 17:16:12 +0900

    ✨feat: #S10P21C101-203 동화책 진행 나레이션 진행중
    Former-commit-id: 5df9e25ee350b1170c4a256a53d48f2c47e843e7

--- frontend/lib/presentation/pages/book/book_progress.dart
+++ frontend/lib/presentation/pages/book/book_progress.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c834d50edb9fa91a35d65e030f914370d610ba25c4e4e588dbda90fd4af9d07
-size 9300
+oid sha256:0d5ee62c8e283a4bfaf32447c8836e9af408a2686e579b92d6a095fdbcac45c4
+size 11634
