2edc5d8295c73f8e4fc2c06fa08067a1dd9a8096
김민석 <kms278711@naver.com>
2024-03-28 17:42:20 +0900

    ✨feat : #S10P22C101-374 이미지 아니면 업로드 안되게 수정, 이메일 검증 로직 추가
    Former-commit-id: 0643099a7ede2c909220afd03175363de5aa65c9

--- backend/src/main/java/com/ssafy/backend/domain/user/service/impl/AuthServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/service/impl/AuthServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05cb79f18a878bdf1fdc1b6e55caeb6cbf755254671b42d360f246488a22b7c4
-size 5596
+oid sha256:9659e3b765a3141f5da05b6f74842bb4b74fe5f7dfb5ed1b81a48e7f234a46b5
+size 5885
