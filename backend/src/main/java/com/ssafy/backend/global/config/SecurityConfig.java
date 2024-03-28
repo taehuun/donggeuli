f2a72865bd8540efb1204e5973ee3e4f35f716ba
김민석 <kms278711@naver.com>
2024-03-28 13:24:33 +0900

    🐛 fix : 웹훅용 결제 api security탐지 제외
    Former-commit-id: 40535b0ad2e86f26cf25930fc8205246785e8dcb

--- backend/src/main/java/com/ssafy/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/backend/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79a982cb9ea68be7e14c749fce02acaa939b2a90f67904d9c45a95102ec138ed
-size 2933
+oid sha256:5f4b9440f940d53c17557f491f97ccb5ae310fd6b2bd416ed9411950678bf073
+size 3042
