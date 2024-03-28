562e0f93adb46732a987015e807e2bf00a2aea1f
김민석 <kms278711@naver.com>
2024-03-28 17:42:20 +0900

    ✨feat : #S10P22C101-374 이미지 아니면 업로드 안되게 수정, 이메일 검증 로직 추가
    Former-commit-id: 204fa72d366578bdd0e6c033aa086134d57d25b9

--- backend/src/main/java/com/ssafy/backend/global/util/S3Util.java
+++ backend/src/main/java/com/ssafy/backend/global/util/S3Util.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcf1e275bbb36c004e48ff342ce1286bb08332b6fee6c900d92d9ccf43fab38e
-size 2441
+oid sha256:d07a7ae7bedf933786240a3ffcef17ee90dc7397a24d93619de89842c124856f
+size 2461
