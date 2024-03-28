e194258fd0105dc8d6407f5919091be6a3e04ed4
강대수 <sore503@naver.com>
2024-03-28 13:34:45 +0900

    ♻refactor #S10P21C101-backend ApprovalService 수정
    Former-commit-id: 83e518bba7019cb31c624cbdce0b3d3305074942

--- backend/src/main/java/com/ssafy/backend/domain/approval/service/impl/ApprovalServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/approval/service/impl/ApprovalServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7c5d294ba361f5887cf920c917f5adc336c7c17de73bf0f9cce5d1145ee4eb7
-size 4361
+oid sha256:8ab987294bd89ffd7bc9cd16eac9e6f8f9044ff0a83157c1d30f3cc8216ebedd
+size 4354
