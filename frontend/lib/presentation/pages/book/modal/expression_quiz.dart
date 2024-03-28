a1b1fa5cbfc2c12e2f8292fa0673350bbb2a4f34
김민주 <ducco705@snu.ac.kr>
2024-03-28 17:58:19 +0900

    :construction: feat: #S10P22C101-207 tflite 모델 손보는중...
    Former-commit-id: 7e18cf49c1db16e5bfccbd6708da569287de781a

--- frontend/lib/presentation/pages/book/modal/expression_quiz.dart
+++ frontend/lib/presentation/pages/book/modal/expression_quiz.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:339c7335eb6ffe7cf4dcc808b5e22cb20cbbca2b93a3bd59fcea86b4a362ef41
-size 7388
+oid sha256:61ea0fe69106bc663b26738468288c02e184ca2b81887b80c3697c49f886fa9b
+size 7629
