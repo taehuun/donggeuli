c94ff818b5a72b537d047890618e523d6dd152ef
김민욱 <minwookkim@naver.com>
2024-03-29 16:30:03 +0900

    ✨feat: #S10P21C101-132 카카오 소셜 로그인 구현중
    Former-commit-id: dec67e25555086e80c87664c138c27a916167e1a

--- frontend/lib/presentation/pages/book/book_progress.dart
+++ frontend/lib/presentation/pages/book/book_progress.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d23f03ccdc4db682a16ea97d03a72f59a8627201f8888bbc15911d414eea70aa
-size 12201
+oid sha256:9ac4d9fb33b9d8e66f808735fc0642515a7f53c01aba5b50cdbb55786efbaf70
+size 12453
