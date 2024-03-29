552658d0954ebf4e005c2745d665a065e7340021
김민주 <ducco705@snu.ac.kr>
2024-03-29 16:14:46 +0900

    :construction: feat: #S10P22C101-207 tflite 모델 손보는중...
    Former-commit-id: 336ae43b9ab751603a22ca5ae9fc00c80e84e167

--- frontend/lib/presentation/pages/book/modal/noword_quiz.dart
+++ frontend/lib/presentation/pages/book/modal/noword_quiz.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:111fb47c292e92e9e539e13192e669f8242acb6716583053139c41bf0bec1eb2
-size 14602
+oid sha256:efda0e4f8c8a28fcb6cc28e8b6abcc0e90f83a8a7a8aacbd2a1f09b6187882a2
+size 14511
