d1732f35f0d7cdde62543cb6a7ded0db3194b144
김민욱 <minwookkim@naver.com>
2024-03-29 09:20:19 +0900

    ✨feat: #S10P21C101-375 배경음악 수정 완료
    Former-commit-id: 9ca1c49bb2f8ad629dd7f4de6dc84672c1ee78f2

--- frontend/lib/core/utils/component/icons/sound_icon.dart
+++ frontend/lib/core/utils/component/icons/sound_icon.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73d4a4b7896d0f9cc265a3687f580970989308ede9b1b7634d5c69e169b2ac59
-size 891
+oid sha256:0cc61714873ca8aa49fe533eeb72bc4f397e28bb77cdc49193b3f9d5f1739408
+size 1102
