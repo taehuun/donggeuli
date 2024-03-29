0ff16a1f225bb208db0afccce3556d69608be4c6
김민주 <ducco705@snu.ac.kr>
2024-03-29 17:46:42 +0900

    :sparkles: feat: #S10P22C101-207 AI OX 모델 테스트 완
    Former-commit-id: bf9403bea7c60ef3d9aced6d366e24e765f8c6ab

--- frontend/lib/presentation/pages/book/modal/noword_quiz.dart
+++ frontend/lib/presentation/pages/book/modal/noword_quiz.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efda0e4f8c8a28fcb6cc28e8b6abcc0e90f83a8a7a8aacbd2a1f09b6187882a2
-size 14511
+oid sha256:329230304daf1dd1e5fe7237ee09be776817c2c9d0613f54acec8a502c1f226f
+size 14506
