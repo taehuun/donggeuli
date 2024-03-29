9b6cdcd1f7c5dadbd18095372e94648d8bb6923b
김태훈 <xogns5@naver.com>
2024-03-29 16:36:18 +0900

    📝docs: #S10P21C101-361 requirement 수정10
    Former-commit-id: 78323d4a5379297d4990909617b468eb1bdf0d2a

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78a6786a523da385b04e809a23511585b8edcf7b5ce6e6b8a8f5c47276a82a15
-size 8571
+oid sha256:d72e875f033f7568f12d118ede217dc1985bb37c239ed7d0cb5d95d53e3761cb
+size 8636
