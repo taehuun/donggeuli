c720f974dbd862923717afa15eaa59da8cee06a1
kms278711 <35741985+kms278711@users.noreply.github.com>
2024-03-30 14:14:26 +0900

    ✨feat: api/educations/{educationId} 에 category 추가
    Former-commit-id: e7c13542f5c212039c751345ae9a81a8273e9a1f

--- backend/src/main/java/com/ssafy/backend/domain/education/dto/response/EducationResponseDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/education/dto/response/EducationResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06d1dc5ac50e9884f3d3b9b55cd351f771d47dfa356b775794f689461fc7244e
-size 877
+oid sha256:5ccb952b1badda3919115fff0109910e97ba9384e9b268575c9956b51eb3126d
+size 1054
