dc3b4db9d456b282de9646dae91ff2ebf52a49e4
kms278711 <35741985+kms278711@users.noreply.github.com>
2024-03-30 14:14:26 +0900

    ✨feat: api/educations/{educationId} 에 category 추가
    Former-commit-id: 309ec38b890ac487ddf265852f67741125ff155d

--- backend/src/main/java/com/ssafy/backend/domain/education/repository/education/EducationCustomRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/education/repository/education/EducationCustomRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e574abbc339366b9636593a32b9d423eba29d1d323a7b6d51d53a99a1406b95e
-size 3520
+oid sha256:871cbe4a318d48f7969e17c723682308a1536caab7f84ab9442d7b41419fdc8e
+size 3549
