92f3ff891513c4bb1f9852f08c1b421b5f744701
김민욱 <minwookkim@naver.com>
2024-03-30 14:17:34 +0900

    �fix: 동화책 진행 시 화면 두번 렌더링 되는거 수정
    Former-commit-id: 972d63d6d3269df3f6d5a994b9f21b6893497691

--- frontend/lib/presentation/pages/book/book_progress.dart
+++ frontend/lib/presentation/pages/book/book_progress.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27f734ae7be29641e4d215fede1ad82e389d2a37dbe59d956c3defbf0aeee7ab
-size 12862
+oid sha256:73ee5f6aa50a3b5a4da674d5d04dc8cf7a12a23fdc976903847bb7b62dfd5632
+size 12631
