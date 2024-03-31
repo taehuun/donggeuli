bf5640d8c1111bf38543c3b49d43f1c3ad4725bf
KangDaeSu <sore503@naver.com>
2024-03-31 21:42:14 +0900

    ♻refactor: #S10P21C101-382 결제 내역 저장 롤백
    Former-commit-id: 2a90ff77d8909a77aa9de263e663b28d0c0ff033

--- backend/src/main/java/com/ssafy/backend/domain/approval/controller/ApprovalController.java
+++ backend/src/main/java/com/ssafy/backend/domain/approval/controller/ApprovalController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8daa288ffd3cf7066b5e0677f433a5188def7650c63bbd343b05d702e3e7c836
-size 1854
+oid sha256:52bfc0fc975060c8255352b57e305040a2570e7dc164fddad79fd1749b11e11b
+size 1533
