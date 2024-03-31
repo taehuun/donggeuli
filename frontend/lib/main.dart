453285e1173cb1e96933bc729517b59d223aa714
김민욱 <minwookkim@naver.com>
2024-03-31 22:34:56 +0900

    카메라 마이크 권한 제일 처음에 받기
    Former-commit-id: 17969c8ade3d66387d43f608bbbd934f64c86d26

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ff06a985c0e171955ff216cd70546503b8316bc6886dd6a96ef6ba8b507fb4c
-size 6667
+oid sha256:dc1382c54b4112bfd42d28b3d64a272a8ae06d7d8204cc7fb7c82bc27b1c166a
+size 6886
