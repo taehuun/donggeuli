7a8a29c49b18e205263abc528ab2a0405081dc20
minseok <kms278711@naver.com>
2024-03-31 21:49:13 +0900

    ✨ feat: traceImagePath api/educations/{educationId}에 추가
    Former-commit-id: 3d5d666d561b6060e3d359e4c2050fa4fa5f6b2f

--- backend/src/main/java/com/ssafy/backend/domain/education/dto/response/EducationResponseDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/education/dto/response/EducationResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ccb952b1badda3919115fff0109910e97ba9384e9b268575c9956b51eb3126d
-size 1054
+oid sha256:e0d1013aae918736f1c5ad89283424a333e7c832417c832ea5ba17d15a209cb7
+size 1177
