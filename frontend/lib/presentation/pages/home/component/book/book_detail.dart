bfc897fcb00103ec88ba6e2f47b6d821acd15302
김민주 <ducco705@snu.ac.kr>
2024-04-01 17:51:21 +0900

    :construction: feat: #S10P22C101-386 이미지 프리로딩 진행중...
    Former-commit-id: f0fdc12d023bde06b71ecd8bb4e67754e26d3a41

--- frontend/lib/presentation/pages/home/component/book/book_detail.dart
+++ frontend/lib/presentation/pages/home/component/book/book_detail.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba010c7cd3ce0ee899a545fa4184bb0b102f3e66d33920199163c260d51613dc
-size 11394
+oid sha256:491ea6556a24e896d4ce8183a5bb9b7acae004a24e812703bf303fd49c802499
+size 11492
--- frontend/lib/presentation/pages/home/component/book/opened_book.dart
+++ frontend/lib/presentation/pages/home/component/book/opened_book.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fe7dce3228ad55da3dfa3e97138550571ccfbe2ed74cf104d257aea26f5d0b3
-size 2608
+oid sha256:bbd7bb4ca83ba515b49731a2afa19460ab168ddd70f491703b1f8403305079e6
+size 2646
