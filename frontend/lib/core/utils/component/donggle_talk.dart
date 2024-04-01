dc48644cd9c472d0360bcb6e0ce8292bf32e41ba
김민주 <ducco705@snu.ac.kr>
2024-04-01 11:51:43 +0900

    :sparkles: feat: #S10P22C101-423 동글이 크기 수정
    Former-commit-id: 10d51a47d9a10212dd4185e1d63deba73a5c9f97

--- frontend/lib/core/utils/component/donggle_talk.dart
+++ frontend/lib/core/utils/component/donggle_talk.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c1759b0a5e021c0fcaac17088d0c3aa767ab867cf799f53cd28a48991236a23
-size 14299
+oid sha256:75b34e668197e89b2ac49e9cad7a990f000ec03e06bd55e1fca79fc19317e676
+size 10771
