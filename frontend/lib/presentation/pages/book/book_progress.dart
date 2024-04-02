c55513068b4b7c274b30d91b74cade4583ca89d7
김민주 <ducco705@snu.ac.kr>
2024-04-02 09:19:39 +0900

    :bug: feat: #S10P22C101-386 버그 픽스
    Former-commit-id: 1c2100003dbc0413296c7fd69129fc44847311fd

--- frontend/lib/presentation/pages/book/book_progress.dart
+++ frontend/lib/presentation/pages/book/book_progress.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c302313db5a18c597299f99f49a829986e42b2c5dc7c45d8ee1a3d0e7f01feb
-size 13644
+oid sha256:9cc5c30b84ec8029488611bad287c71e3785dd13a0780814ebebf07b0d8c1dfb
+size 13594
