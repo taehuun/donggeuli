1c83d6f59508fd3007440b6b24676693c3d06282
김태훈 <xogns5@naver.com>
2024-04-02 20:23:40 +0900

    ✨feat: index.html생성
    Former-commit-id: 9304b5149c732fda88734636667dbb3aa6b9c3ab

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10f5d422aa42726305f8ea2f8051a94bfaa16f9e6b91e6ce054031adcadf0efe
-size 8929
+oid sha256:4c06d3b1e37b43f4e417ef2a4e2657a4eedc04f9101ddf14a95444e5fca14ac5
+size 9019
