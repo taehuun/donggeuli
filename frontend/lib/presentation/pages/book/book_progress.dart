506f7ffa52577d07ef901bac72015011848c9d6a
김민욱 <minwookkim@naver.com>
2024-04-02 11:24:55 +0900

    모달 두번 나오는거 수정
    Former-commit-id: bd052b33efb6885b8797f06e169d5f6c68566924

--- frontend/lib/presentation/pages/book/book_progress.dart
+++ frontend/lib/presentation/pages/book/book_progress.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cc5c30b84ec8029488611bad287c71e3785dd13a0780814ebebf07b0d8c1dfb
-size 13594
+oid sha256:f527c1b72529264862de33e8c60af9f542b05c525d3bf001f8f5b48c2ceb2ea7
+size 13660
