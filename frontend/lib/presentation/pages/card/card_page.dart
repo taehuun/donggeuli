0f61082606b33e0fec6b0dc42d2ddf8d064505c8
김민주 <ducco705@snu.ac.kr>
2024-04-02 21:06:32 +0900

    :bug: bug: #S10P22C101-424 이미지 프리로딩 수정, 오디오 프리로딩 구현
    Former-commit-id: 649f190f16f0d0378f56437b9e600c4703a64b24

--- frontend/lib/presentation/pages/card/card_page.dart
+++ frontend/lib/presentation/pages/card/card_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc9d2ed92f8f842288ca1eaf84f69043f3633cd160d20806facbc123537e790f
-size 4610
+oid sha256:a9b59de8ac4c3b548650f4271492a34d44873be6472c63af891983ae6aa9a47c
+size 6388
