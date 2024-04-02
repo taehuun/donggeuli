58277cde0d4b7c0bc436477d2f1bac09cfd1b38b
김민욱 <minwookkim@naver.com>
2024-04-02 17:43:30 +0900

    로딩화면 수정
    Former-commit-id: 5f21a367ceede113225ff43f4a16ebb9b870469b

--- frontend/lib/core/utils/component/loading_screen.dart
+++ frontend/lib/core/utils/component/loading_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cfc5c9c9b44f996bc22fb5640e3a573cdea4d57a39b6167c262446edcfd8dd2
-size 22549
+oid sha256:a859dc896772405cd2a37161d6ecb1cc5b6b26303c7af692714ac27bace64812
+size 22516
