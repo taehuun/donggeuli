43764be85d22548f1ef9bac031b708059c54e93b
김민욱 <minwookkim@naver.com>
2024-04-02 23:59:39 +0900

    feature: 스플래시 페이지 애니메이션
    Former-commit-id: 7db1fffd3eadfc967817437828fcaade31ce8e4b

--- frontend/lib/presentation/pages/splash/splash_page.dart
+++ frontend/lib/presentation/pages/splash/splash_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e136caf2b0dd890678db553f0443ae1c82b5ea2c1761e2682162a40ecef1ee3b
-size 3428
+oid sha256:91bffe07f091514f14af25ed1339ab0a7423629c3db53866162179c69ada6824
+size 25741
