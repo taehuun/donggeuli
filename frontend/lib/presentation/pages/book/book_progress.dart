aa7bb59e0cfabb259704ffa4273ef56f9efefa4f
김민욱 <minwookkim@naver.com>
2024-04-03 10:55:10 +0900

    이전버튼 누르면 자동재생 멈추기 수정
    Former-commit-id: f7129395571306147117b9d12089ab7ca45b1a22

--- frontend/lib/presentation/pages/book/book_progress.dart
+++ frontend/lib/presentation/pages/book/book_progress.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:317b51163ed50b3e63458f8c67389fb8c1fdb6454ec404304d01c1d5bc1c4b60
-size 14124
+oid sha256:70ad5fd4ad92f233c03f0b32e4a04302dd5083a5e31218a7c4f4aa7dac7d128e
+size 14146
