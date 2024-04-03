3ee9533637655f3f8104f7acfe3543b28c61d335
김민주 <ducco705@snu.ac.kr>
2024-04-03 11:02:13 +0900

    버그 수정
    Former-commit-id: 7735215bd09f6175daff6424dc26d6eb1e297698

--- frontend/lib/presentation/pages/book/modal/book_finish_modal.dart
+++ frontend/lib/presentation/pages/book/modal/book_finish_modal.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:828b981a36e16a59d6422a25835dacbba44fe949c24139f73f42b2aec26d0eb7
-size 4525
+oid sha256:c2bf8b940eaf5b7c9ae58ad25796de5776ee05e4175b39718bb3df1c3bf08964
+size 4711
